de3e1107ea1c0cebd5b1450adf6c32fcb29a854a
dickycom <dickycom1@gmail.com>
2021-09-09 23:42:33 -0400

    Support for non number ports (#2)
    Fixed some processes not showing up. The script was fixed and now they're showing with the correct port number.

--- app/services/processreader.ts
+++ app/services/processreader.ts
@@ -5,7 +5,7 @@ export type Process = {
 	id: number;
 	command: string;
 	isWSL: boolean;
-}
+};
 
 /**
  * Returns a list of processes using the TCP ports in the specified range
@@ -13,7 +13,7 @@ export type Process = {
  * @param toPort
  * @param checkWslProcesses
  */
-export async function getProcesses(fromPort=MIN_PORT_NUMBER, toPort=MAX_PORT_NUMBER, checkWslProcesses = false): Promise<Process[]> {
+export async function getProcesses(fromPort = MIN_PORT_NUMBER, toPort = MAX_PORT_NUMBER, checkWslProcesses = false): Promise<Process[]> {
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore See https://www.electronjs.org/docs/tutorial/process-model#preload-scripts
 	return window.process.getProcesses(fromPort, toPort, checkWslProcesses);
