6897ca9e64181889e8bd206c4c06775a30dbc575
henryjw <3443648+henryjw@users.noreply.github.com>
2021-11-27 12:21:26 -0500

    Add separate scripts to build Mac x64 and ARM64

--- scripts/apple/config.ts
+++ scripts/apple/config.ts
@@ -14,16 +14,16 @@ const env = (envVariableName: string, required = true): string => {
 	return value;
 };
 
+if (missingVariables.length > 0) {
+	throw new Error(`ERROR: Missing environment variables: \n${missingVariables.join("\n")}`);
+}
+
 const config =  {
 	appBundleId: env("APPLE_BUNDLE_ID"),
-	appPath: env("APPLE_APP_PATH"),
+	appPaths: env("APPLE_APP_PATHS").split(";"),
 	appleId: env("APPLE_ID"),
 	appleIdPassword: env("APPLE_ID_PASSWORD"),
 	teamId: env("APPLE_TEAM_ID")
 };
-
-if (missingVariables.length > 0) {
-	throw new Error(`ERROR: Missing environment variables: \n${missingVariables.join("\n")}`);
-}
 
 export default config;
--- scripts/apple/sign-package.ts
+++ scripts/apple/sign-package.ts
@@ -5,16 +5,25 @@ import { notarize } from "electron-notarize";
 import config from "./config";
 
 (async function signTasks () {
-	console.debug("Notarizing app...");
+	// Notarize multiple apps when there different architectures supported
+	const promises = config.appPaths.map(async appPath => {
+		console.debug(`Notarizing ${appPath}`);
+		await notarize({
+			appPath,
+			appBundleId: config.appBundleId,
+			appleId: config.appleId,
+			appleIdPassword: config.appleIdPassword,
+			teamId: config.teamId,
+			tool: "notarytool",
+		});
 
-	// @ts-ignore
-	await notarize({
-		appBundleId: config.appBundleId,
-		appPath: config.appPath,
-		appleId: config.appleId,
-		appleIdPassword: config.appleIdPassword,
-		teamId: config.teamId,
-		tool: "notarytool",
-	});
-	console.debug("Notarization done.");
+		console.debug(`Notarization done for ${appPath}.`);
+	})
+
+	try {
+		await Promise.all(promises);
+		console.debug("Notarization complete for all apps.");
+	} catch(err) {
+		console.error("Notarization failed", err);
+	}
 })();
